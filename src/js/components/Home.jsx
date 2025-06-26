48fdb037d5b60072097cd5d8d67eab4bb5093311
Incerteza88 <info@incerteza.es>
2025-06-26 18:31:08 +0000

    fetch gatos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const API = 'https://cataas.com'
+
+	const [imagen, setImagen] = useState(null)    //para crear una variable para guardar la imagen
+	const [listaGatos, setListaGatos] = useState(null) //para crear una variable de una lista de gatos 
+
+
+	function getCat() {
+		fetch(API + "/cat")
+			.then((response) => response.blob())
+			.then((dataImagen) => setImagen(URL.createObjectURL(dataImagen)))   //URL.createObjectURL(dataImagen) = convierte la imagen en una URL
+
+	}
+
+	function getListaGatos() {
+		fetch(API + "/api/cats")
+			.then((response) => response.json())
+			.then((dataLista) => setListaGatos(dataLista))    // la lista de gatos esta en dataLista pero la guardamos dentro de setListaGatos (setListaGatos es solo para guardar pero lo llamamso con listaGatos)
+	}
+
+	useEffect(() => {
+		getCat()
+		getListaGatos()
+	}, [])							//para que se actualice cada vez que se renderice el componente
+
+	return (
+		<>
+			{imagen && <img src={imagen} /> //&& para comprobar si la variable esta vacía, si está vacía no se muestra
+			}
+			{listaGatos && listaGatos.map((item, index) => (   //item es el objeto que recorre en ese momento e index es el indice el numero
+				<li>{item.id} contiene las siguiente etiquetas:
+					{item.tags.map((objeto, indice) => (		//ponemso oobjeto e indicie porque item, index ya esta usado pero es lo mismo
+						<li>{objeto}</li>
+					))}
+				</li>
+			))}
+		</>
 	);
 };
 
